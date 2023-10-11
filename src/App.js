abf7c3fcb221bfc5519fb5fb8139247cc1b0cf54
Rameil Samuel <rameilsamuel@Rameils-Mac-Pro.local>
2023-10-11 10:18:03 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Html5QrcodePlugin from "./Html5QrcodePlugin";
 
 function App() {
+  const onNewScanResult = (decodedText, decodedResult) => {
+    // Handle the result here.
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Web Barcode Scanner</p>
+        <Html5QrcodePlugin
+          fps={10}
+          qrbox={250}
+          disableFlip={false}
+          qrCodeSuccessCallback={onNewScanResult}
+        />
         <a
           className="App-link"
           href="https://reactjs.org"
